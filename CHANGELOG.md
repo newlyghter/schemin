18c8925d79e5a294eba27549fa7c4eba37533f65
newlyghter <stigma-peeling-0r@icloud.com>
2024-08-14 23:54:04 +0200

    parsing atoms, strings, numbers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,13 @@
 # Revision history for schemin
 
-## 0.1.0.0 -- YYYY-mm-dd
+## 0.1.0.0 -- 2024-08-14
 
 * First version. Released on an unsuspecting world.
 
 ## 0.1.0.1 -- 2024-08-14
 
 * Fixed parser choking on whitespaces.
+
+## 0.1.1.0 -- 2024-08-14
+
+* Added parsing of Atoms, Strings and Numbers.
--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,40 @@ module Main where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+  deriving Show
+
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '"'
+  x <- many (noneOf "\"")
+  _ <- char '"'
+  return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -10,9 +44,9 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right val -> "Found value" ++ show val
 
 main :: IO ()
 main = do
